bf299c4168d4f5344f4350839b99433669baabd8
AngelHoyos <hoyosboli20@gmail.com>
2024-09-07 00:38:04 -0500

    Creacion del Middelware y aperturas de los CORS

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,21 +1,28 @@
 import { Inject, Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 
-
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
-   use (req:Request, res:Response, next:NextFunction){
-    res.header('Access-Control-Allow-Origin', 'https://websitejhass.netlify.app');
-    res.header('Access-Control-Allow-Methods','GET,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+   use(req: Request, res: Response, next: NextFunction) {
+    const allowedOrigins = [
+        'https://websitejhass.netlify.app', // Producción
+        'http://localhost:5173'             // Desarrollo local
+    ];
+    
+    const origin = req.headers.origin as string;
+    
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
 
-
-    if (req.method==='OPTIONS') {
+    if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
     }
 
     next();
+   }
 }
-}
-
