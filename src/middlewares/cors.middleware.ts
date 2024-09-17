1f25457a61e8b9684ccbacf6687a05e940bdac2a
AngelHoyos <hoyosboli20@gmail.com>
2024-09-17 16:09:15 -0500

    Actualizacion del archivo cors.middelware.ts

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,25 +1,28 @@
-import { Inject, Injectable, NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
-
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
-    use (req:Request, res:Response, next:NextFunction){
-        res.header('Access-Control-Allow-Origin', 
-            //'https://websitejhass.netlify.app
-            // 'http://localhost:5173'
-            '*'
-        );
-        res.header('Access-Control-Allow-Methods','GET,PUT,PATCH,POST,DELETE');
-        res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    use (req: Request, res: Response, next: NextFunction) {
+        // Configura los orígenes permitidos según el entorno
+        const allowedOrigins = ['http://localhost:5173', 'https://eclectic-malabi-5700c0.netlify.app'];
+
+        // Verifica si el origen de la solicitud está en la lista de orígenes permitidos
+        const origin = req.headers.origin;
+        if (allowedOrigins.includes(origin as string)) {
+            res.header('Access-Control-Allow-Origin', origin);
+        } else {
+            res.header('Access-Control-Allow-Origin', '*'); // Para permitir cualquier origen en caso de no coincidir
+        }
+
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
         res.header('Access-Control-Allow-Credentials', 'true');
-        
-        
-        if (req.method==='OPTIONS') {
+
+        if (req.method === 'OPTIONS') {
             return res.sendStatus(204);
         }
-        
-        next();
 
+        next();
     }
 }
